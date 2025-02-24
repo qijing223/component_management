b3dde713b862edc68d4771291fef13c810c36d58
ZixuanShen <127663957+moseicshen@users.noreply.github.com>
2025-02-24 11:16:52 -0600

    init

---
+++ src/test/java/com/lot/backend/IoTBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
