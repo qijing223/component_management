5338b0c9ba612469bd78f35179d63b89b8a044dd
ZixuanShen <127663957+moseicshen@users.noreply.github.com>
2025-03-14 10:55:08 -0500

    security problem

--- src/main/java/com/lot/server/login/model/UserLoginDTO.java
+++ src/main/java/com/lot/server/login/model/UserLoginDTO.java
@@ -8,9 +8,9 @@ import java.io.Serializable;
 @Data
 @Schema(description = "User Login DTO")
 public class UserLoginDTO {
-    @Schema(description = "User Id")
-    private Integer userId;
-    @Schema(description = "User Name")
+//    @Schema(description = "User Id")
+//    private Integer userId;
+    @Schema(description = "User Name(Employee ID?)")
     private String username;
     @Schema(description = "Password")
     private String password;
